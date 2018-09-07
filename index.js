31c9188e0076a387e1ac685169977969d704c8ad
vanceleon <vanceleon44@gmail.com>
2018-09-07 12:38:35 -0400

    put for actions is completed  fixing post operation

--- index.js
+++ index.js
@@ -77,10 +77,47 @@ server.get("/projects/:id", (req, res) => {
 });
 
 
-// server.post()
+server.post("/projects/:id", (req, res, next) => {
+    const id = req.params.id;
+    const project = req.body;
+
+    projects.insert(id)
+        .then(project => {
+            if(project) {
+                res.status(200).json({ project });
+            }else {
+                res.status(400).json({message: "Error couldn't provide post content"});
+            }
+        })
+        .catch(err => {
+            console.log("error", err);
+            res.status(500).json({ messgae: "Error getting the information to post" });
+          });
+});
 
 
-
+server.put("/actions/:id", (req, res) => {
+  const description = req.body.description;
+  const id = req.params.id
+  if(description) {
+    actions.update(id, req.body)
+    .then(actions => {
+      if (actions) {
+        res.status(200).json({ actions });
+      }else {
+        res.status(404).json({message: "ID doesn't exist to update"});
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({
+        message: "Error getting the information for each character"
+      });
+    });
+} else {
+  res.status(400).json({ message: "Please provide text" });
+  }
+});
 
 
 
